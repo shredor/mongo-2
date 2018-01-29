00695fb2f1ace4a5dd4e56bdab762e7466f6991c
Anton <shredor@icloud.com>
2018-01-29 23:11:27 +0300

    110 done

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
+const routes = require('./routes/routes')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+app.use(bodyParser.json())
+routes(app)
 
 module.exports = app
--- test/app_test.js
+++ test/app_test.js
@@ -5,7 +5,7 @@ const app = require('../app')
 describe('the express App', () => {
   it('handles a request to root route', done => {
     request(app)
-      .get('/')
+      .get('/api')
       .end((err, response) => {
         assert(!err)
         assert(response.body.hi === 'there')
