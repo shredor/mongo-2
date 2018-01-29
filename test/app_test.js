e675eb6d1ef1c5da0b7e714089c435525260da4e
Anton <shredor@icloud.com>
2018-01-29 22:42:46 +0300

    add root route

---
+++ test/app_test.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+const request = require('supertest')
+const app = require('../app')
+
+describe('the express App', () => {
+  it('handles a request to root route', done => {
+    request(app)
+      .get('/')
+      .end((err, response) => {
+        assert(!err)
+        assert(response.body.hi === 'there')
+        done()
+      })
+  })
+})
